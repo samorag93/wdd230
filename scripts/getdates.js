327685255391af7d8045e3f2b14ed4cfcb906dbd
samorag93 <moronyag90@gmail.com>
2024-01-25 20:54:33 +0100

    week 03

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,3 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var lastModifiedDate = document.lastModified;
     document.getElementById('lastModified').textContent = 'Last modified: ' + lastModifiedDate;
 });
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
