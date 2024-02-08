195337b241c053dedd7ffa41376c547a1bdab41b
samorag93 <moronyag90@gmail.com>
2024-02-08 21:38:59 +0100

    chmber weeok 05

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,18 +1,34 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Copyright year
-    var currentYear = new Date().getFullYear();
-    document.getElementById('year').textContent = currentYear;
+    document.addEventListener('DOMContentLoaded', function () {
+        // Copyright year
+        var currentYear = new Date().getFullYear();
+        document.getElementById('year').textContent = currentYear;
 
-    // Last modified date
-    var lastModifiedDate = document.lastModified;
-    document.getElementById('lastModified').textContent = 'Last modified: ' + lastModifiedDate;
-});
+        // Last modified date
+        var lastModifiedDate = document.lastModified;
+        document.getElementById('lastModified').textContent = 'Last modified: ' + lastModifiedDate;
+    });
 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+    const mainnav = document.querySelector('.navigation')
+    const hambutton = document.querySelector('#menu');
 
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('show');
+        hambutton.classList.toggle('show');
+    });
+
+    const modeButton = document.querySelector("#mode");
+    const main = document.querySelector("main");
+
+    modeButton.addEventListener("click", () => {
+        if (modeButton.textContent.includes("🕶️")) {
+            main.style.background = "black";
+            main.style.color = "white";
+            modeButton.textContent = "🔆";
+        } else {
+            main.style.background = "white";
+            main.style.color = "black";
+            modeButton.textContent = "🕶️";
+        }
+    });
+
