8dba9853eb2e8058f4e2230c8d116dd1247efb3d
samorag93 <moronyag90@gmail.com>
2024-02-16 14:13:23 +0100

    ok1

--- chamber/scripts/windChill.js
+++ chamber/scripts/windChill.js
@@ -1,20 +1,20 @@
 function calculateWindChill(temperature, windSpeed) {
-    // Verificar si los valores cumplen con los límites de especificación
+    
     if (temperature <= 50 && windSpeed > 3.0) {
-        // Calcular el factor de sensación térmica
+        
         var windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
-        return windChill.toFixed(1); // Redondear a una décima
+        return windChill.toFixed(1); 
     } else {
         return "N/A";
     }
 }
 
-// Obtener la temperatura y la velocidad del viento del HTML
+
 var temperature = parseFloat(document.getElementById("temperature").innerText);
 var windSpeed = parseFloat(document.getElementById("windSpeed").innerText);
 
-// Calcular el factor de sensación térmica
+
 var windChill = calculateWindChill(temperature, windSpeed);
 
-// Mostrar el factor de sensación térmica en el HTML
+
 document.getElementById("windChill").innerText = "Wind Chill: " + windChill + "°F";
