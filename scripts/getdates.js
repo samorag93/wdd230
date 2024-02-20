87048f5e96b41bac6557d8f9e16978e9113e617b
samorag93 <moronyag90@gmail.com>
2024-02-20 13:03:50 +0100

    homepageweek07

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -17,3 +17,18 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+//step 1
+const visitsDisplay = document.querySelector(".visits");
+//step 2
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+//step 3
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+//step 4
+numVisits++;
+//step 5
+localStorage.setItem("numVisits-ls", numVisits);
